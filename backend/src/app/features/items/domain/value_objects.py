b17867341db1f078d1dd182fc66cf87eaf112102
Sam Cork <sam57719@gmail.com>
2026-01-11 23:57:56 +0000

    Remove `kw_only=True` from ItemID dataclass for simplicity

--- backend/src/app/features/items/domain/value_objects.py
+++ backend/src/app/features/items/domain/value_objects.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from app.shared.domain.value_objects import DomainID
 
 
-@dataclass(frozen=True, slots=True, kw_only=True)
+@dataclass(frozen=True, slots=True)
 class ItemID(DomainID):
     """Value Object for Item IDs."""
 
